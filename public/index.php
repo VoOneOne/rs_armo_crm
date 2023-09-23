75a4669954cdbca0bdcf34739a94242092756293
VladimirG <golovanov.v-mir@yandex.ru>
2023-09-23 22:09:09 +0300

    определены маршруты для получения и отправки формы

--- public/index.php
+++ public/index.php
@@ -1,2 +1,21 @@
 <?php
-    phpinfo();
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Vooneone\RsArmoCrm\Controllers\FormController;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/app', function () {
+    $formController = new FormController();
+    return $formController->index();
+});
+
+$app->post('/app', function (Request $request) {
+    $formController = new FormController();
+    return $formController->insert($request);
+});
+
+$app->run();
